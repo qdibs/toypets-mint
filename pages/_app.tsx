153b30e674fc63da54bcebbcd68e94ec944ff5ba
Qdibs <72762489+qdibs@users.noreply.github.com>
2022-12-18 21:02:52 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
       <Head>
-        <title>thirdweb NFT Drop Minting Customizable Page</title>
+        <title>Mint ToyPets</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
